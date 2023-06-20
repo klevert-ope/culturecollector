0832cf4342c74bdb8ad6a006896722de509f7201
Klevert Opee <klevertope@gmail.com>
2023-06-21 01:32:22 +0300

    gsap

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,9 @@ import { gsap } from "gsap";
 
 const Intro = () => {
   useEffect(() => {
-    const introduction = gsap.timeline();
+    let timeline = gsap.timeline();
 
-    introduction
+    timeline
       .fromTo(
         "#culture",
         { opacity: 0, y: 40 },
@@ -21,7 +21,6 @@ const Intro = () => {
         { opacity: 0, x: -10 },
         { opacity: 1, x: 0, duration: 2, ease: "sine.in" }
       )
-
       .to(["#culture", "#collector", "#quote1"], {
         opacity: 0,
         duration: 1,
@@ -36,7 +35,6 @@ const Intro = () => {
         stagger: 0.2,
         onComplete: showDraggable,
       })
-
       .from("#draggable", {
         opacity: 0,
         y: 10,
@@ -44,7 +42,6 @@ const Intro = () => {
         ease: "sine.in",
         onComplete: showFooter,
       })
-
       .from("#footer", {
         opacity: 0,
         X: 20,
@@ -64,13 +61,15 @@ const Intro = () => {
     function showFooter() {
       document.getElementById("footer").style.display = "block";
     }
+
+    // Return a cleanup function to cancel the animations
+    return () => {
+      timeline.kill();
+    };
   }, []);
 
   return (
-    <section
-      id="welcome"
-      className=" h-screen flex flex-col justify-center items-center"
-    >
+    <section id="welcome" className=" max-h-screen overflow-hidden">
       <div>
         <div className=" absolute top-10 right-5">
           <p
@@ -81,13 +80,11 @@ const Intro = () => {
             souls, and preserves the essence of our culture.
           </p>
         </div>
-        <div className=" flex flex-row justify-center font-Technor font-extrabold text-4xl lg:text-6xl xl:text-7xl">
-          <div>
+        <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <div className=" flex flex-row font-Technor font-extrabold text-4xl lg:text-6xl xl:text-7xl">
             <h1 id="culture" className=" text-red-600 opacity-0">
               Culture
             </h1>
-          </div>
-          <div>
             <h1 id="collector" className=" text-blue-700 opacity-0">
               Collector
             </h1>
